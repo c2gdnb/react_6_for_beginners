811278cde0c226dd849ea88954f7fa6b06c3b85a
c2gdnb <neonx2007@gmail.com>
2022-08-28 17:22:45 +0300

    [05] add currency-converter

--- currency-converter/src/App.js
+++ currency-converter/src/App.js
@@ -42,7 +42,7 @@ function App() {
         console.log(err);
         alert(err);
       });
-  }, [onChangeFromPrice]);
+  }, [onChangeFromPrice, onChangeToPrice]);
 
   useEffect(() => {
     onChangeFromPrice(fromPrice);
