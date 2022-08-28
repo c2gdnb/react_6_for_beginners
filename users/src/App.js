2a5a9f65525290b132d8294749c7b23ef79cc5cc
c2gdnb <neonx2007@gmail.com>
2022-08-28 13:28:13 +0300

    [04] add users

--- users/src/App.js
+++ users/src/App.js
@@ -1,15 +1,65 @@
-import React from 'react';
-import './index.scss';
-import { Success } from './components/Success';
-import { Users } from './components/Users';
+import { useEffect, useState } from "react";
+import "./index.scss";
+import { Success } from "./components/Success";
+import { Users } from "./components/Users";
 
-// Тут список пользователей: https://reqres.in/api/users
+const API_URL = "https://reqres.in/api/users/";
 
 function App() {
+  const [users, setUsers] = useState();
+  const [invites, setinvites] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [success, setSuccess] = useState(false);
+  const [searchValue, setsearchValue] = useState("");
+
+  useEffect(() => {
+    fetch(API_URL)
+      .then((res) => res.json())
+      .then((json) => {
+        setUsers(json.data);
+      })
+      .catch((err) => {
+        console.log(err);
+        alert("Error response");
+      })
+      .finally(() => setIsLoading(false));
+  }, []);
+
+  const onChangeSearchValue = (e) => {
+    setsearchValue(e.target.value);
+  };
+
+  const onClickInvite = (id) => {
+    if (invites.includes(id)) {
+      setinvites((prev) => prev.filter((_id) => _id !== id));
+    } else {
+      setinvites((prev) => [...prev, id]);
+    }
+  };
+
+  const onClickSendInvites = () => {
+    setSuccess(true);
+  };
+
+  const onClickBackBtn = () => {
+    setSuccess(false);
+  };
+
   return (
     <div className="App">
-      <Users />
-      {/* <Success /> */}
+      {success ? (
+        <Success onClickBackBtn={onClickBackBtn} count={invites.length} />
+      ) : (
+        <Users
+          onChangeSearchValue={onChangeSearchValue}
+          searchValue={searchValue}
+          items={users}
+          isLoading={isLoading}
+          invites={invites}
+          onClickInvite={onClickInvite}
+          onClickSendInvites={onClickSendInvites}
+        />
+      )}
     </div>
   );
 }
